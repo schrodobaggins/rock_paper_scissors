cb021cc2ab3eb38ee1b9dbcdbe26328abd891c78
Michael Schroeder <magicalmikely@gmail.com>
2022-03-29 14:03:49 -0700

    completed rough game logic

--- app.js
+++ app.js
@@ -3,26 +3,53 @@
 // Michael Schroeder
 
 
-// Create eventListeners/documentQueries/other variables
-// const choice = document.querySelectorAll(".choice"); // querySelectorAll because multiple choices
+// global instance variables
 
 const choices = ["rock","paper","scissors"];
 
 
+// Actual game logic
 
-// Create actual game logic
-
-function computerPlayer(){
-    return console.log("computerPlay")
+function computerPlay(choices){
+        // get random index value
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        // get random item
+        const item = choices[randomIndex];
+        console.log(item);
+        return item;
 }
 
-function playRound(playerSelection, computerSelection){
-    playerSelection = String.downcase;
 
-    return "you Lose! Paper beats Rock"
+function playRound(playerSelection, computerSelection){
+    // downcase string
+    if(playerSelection == 'rock' && computerSelection == 'paper'){
+        return "you lose";
+        }
+    if(playerSelection == 'scissors' && computerSelection == 'rock'){
+        return "you lose";
+        }
+    if(playerSelection == 'paper' && computerSelection == 'scissors'){
+        return "you lose";
+        }
+    else{
+        return "you win!"
+    }
+    // if(playerSelection == 'paper' && computerSelection == 'rock'){
+    //     return "you win";
+    //     }
+    // if(playerSelection == 'paper' && computerSelection == 'rock'){
+    //     return "you win";
+    //     }   
 };
+
+const playerSelection = "rock";
+const computerSelection = computerPlay(choices);
+console.log(playRound(playerSelection, computerSelection));
+
 
 function game(){
     // Remember loops? This is a great opportunity to use one to play those five rounds:
-    playRound();
+    for(let i = 0; i < 5; i++){
+        playRound();
+    }
 }
